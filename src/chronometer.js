0b7735587aab8b84055838e868f0771c88f429fc
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-10-17 15:37:09 +0200

    lab1 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    // if(printTimeCallback)
+    // {
+      this.intervalId = setInterval(()=>{
+        this.currentTime++;
+        if(printTimeCallback)
+          printTimeCallback();
+      },1000)
+    // }
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let twoDigitStr = ""
+    if(value < 10)
+      twoDigitStr = "0" + value.toString();
+    else
+      twoDigitStr = value.toString();
+    return twoDigitStr
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
+    
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    let formatTime = `${minutes}:${seconds}`;
+    return formatTime;
   }
 }
